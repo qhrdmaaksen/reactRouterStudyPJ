91f1382bd0116d483afba7049dcab193a58b0c89
vitamin777 <rlaalsdn8@gmail.com>
2022-09-02 22:00:12 +0900

    feat : 상품의 상품상세보기를 products 의 경로에서만 볼 수 있도록 설정
    -App.js 에서 Switch 를 활용해 Route 를 랩핑하여 가장 먼저 매칭되는 라우트만 동시에 활성화되도록함
    -exact 를 products 경로가있는 path 에 속성을 붙여줘서 리액트 라우터에 정확히 일치하는 경우에만 전환해주도록함
    
    -기능 정상 확인 커밋

--- src/App.js
+++ src/App.js
@@ -1,26 +1,27 @@
-import { Route } from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
 import Welcome from './pages/Welcome';
 import Products from './pages/Products';
-import MainHeader from './components/MainHeader'
-import ProductDetail from './pages/ProductDetail'
+import MainHeader from './components/MainHeader';
+import ProductDetail from './pages/ProductDetail';
 
 function App() {
   return (
     <div>
       <MainHeader />
       <main>
-
-        <Route path="/welcome">
-          {/*url 뒤에 /Welcome 의 경로가 붙을 경우에만 Welcome 표시*/}
-          <Welcome />
-        </Route>
-        <Route path="/products">
-          <Products />
-        </Route>
-        {/*-동적 라우트 path /example/:productId 로 :동적경로 세그먼트로 이페이지가 로드되어야하는 전체 경로는 다음과 같을거라고 라우터에게 알려주는것*/}
-        <Route path='/products/:productId'>
-          <ProductDetail />
-        </Route>
+        <Switch>
+          <Route path="/welcome">
+            {/*url 뒤에 /Welcome 의 경로가 붙을 경우에만 Welcome 표시*/}
+            <Welcome />
+          </Route>
+          <Route path="/products" exact>
+            <Products />
+          </Route>
+          {/*-동적 라우트 path /example/:productId 로 :동적경로 세그먼트로 이페이지가 로드되어야하는 전체 경로는 다음과 같을거라고 라우터에게 알려주는것*/}
+          <Route path="/products/:productId">
+            <ProductDetail />
+          </Route>
+        </Switch>
       </main>
     </div>
   );
